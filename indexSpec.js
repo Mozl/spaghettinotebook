6cb41e3cdab9d088688b1e38104ac13ce3b85d5a
Jordan Brooks <jorbrooks92@gmail.com>
2018-05-01 13:34:05 +0100

    extract testing functionality to seperate file

--- indexSpec.js
+++ indexSpec.js
@@ -1,16 +1,5 @@
 (function runTests() {
-  var assert = {
-    isTrue: function(assertionToCheck) {
-      if (!assertionToCheck) {
-        throw new Error ("Assertion failed: " + assertionToCheck + " is not truthy");
-      }
-    }
-  };
-
-// ***********************************************
-
-
-
+  
   (function newNote() {
     var note = new Note('this is an example note that has more than 20 characters becasuse it should');
     assert.isTrue(typeof note.title === "string");
