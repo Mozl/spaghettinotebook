87daee53231d4b4142b02f66daa39bc2162c58aa
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-05-01 16:49:02 +0100

    minor changes

--- notemodal.js
+++ notemodal.js
@@ -1,10 +1,10 @@
 var modal = document.getElementById('myModal');
 var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
+var closeButton = document.getElementsByClassName("close")[0];
 btn.onclick = function() {
     modal.style.display = "block";
 };
-span.onclick = function() {
+closeButton.onclick = function() {
     modal.style.display = "none";
 };
 window.onclick = function(event) {
