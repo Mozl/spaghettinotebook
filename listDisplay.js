5b3d5b709c96f2be6fbe8993dffc9172fd17c267
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-05-02 14:46:06 +0100

    added Add button functionality

--- listDisplay.js
+++ listDisplay.js
@@ -1,5 +1,6 @@
 function makeList(notebook) {
   var list = document.getElementById('theList');
+  list.innerHTML = "";
   var i;
   for (i = 0; i < notebook.allNotes.length; i++) {
     var listItem = document.createElement("li");
