26b95ca5bcb1a12548a622fda67aa6c68c4c12d9
Louis Moselhi <louismoselhi@MacBook-Pro-5.local>
2018-05-04 12:04:34 +0100

    refactor of listDisplay

--- listDisplay.js
+++ listDisplay.js
@@ -1,28 +1,26 @@
 function makeList(notebook) {
-  var list = document.getElementById('theList');
-  list.innerHTML = "";
-  var i;
-  for (i = 0; i < notebook.allNotes.length; i++) {
-    var listItem = document.createElement("li");
+  appendNotesTo(resetListContainer());
+}
 
-    listItem.innerHTML = notebook.allNotes[i].title;
-    list.appendChild(listItem);
-  }
-  var all = document.getElementsByTagName('LI');
-  var items = Array.from(all);
-  
-  
-  items.forEach(function(item) {
-    item.onclick = function(){
-    var modal = document.getElementById('modalText');
-    var modallll = document.getElementById('myModal');
-    modal.innerHTML = modalText.innerHTML;
-    modallll.style.display = "block";
-    console.log(modalText);
+function resetListContainer() {
+  var listContainer = document.getElementById('theList');
+  listContainer.innerHTML = "";
+  return listContainer;
+}
+
+function appendNotesTo(listContainer){
+  for (var i = 0; i < notebook.allNotes.length; i++) {
+    var currentNote = notebook.allNotes[i];
+
+    var listContainerItem = document.createElement("li");
+    listContainerItem.innerHTML = currentNote.title;
+    listContainerItem.onclick = function() {
+      var modal = document.getElementById('modalText');
+      var modallll = document.getElementById('myModal');
+
+      modal.innerHTML = modalText.innerHTML;
+      modallll.style.display = "block";
     };
-  });
-
-
-
-
+    listContainer.appendChild(listContainerItem);
+  }
 }
